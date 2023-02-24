d6d7a1403eaceaefb7bea1d1ee640f967c1e5a8b
FLP <42muhammedmustafa@gmail.com>
2023-02-24 16:22:54 +0200

    Logout functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,24 @@ exports.login = async (req, res, next) => {
   }
 };
 
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = async (_req, res, next) => {
+  try {
+    res.cookie("token", "none", {
+      expires: new Date(Date.now() + 10 * 1000),
+      httpOnly: true,
+    });
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
 // @access    Private
