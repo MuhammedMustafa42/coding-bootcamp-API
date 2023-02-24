4b8f833bf7ab20e838c1ef8067bd25ce944333e5
FLP <42muhammedmustafa@gmail.com>
2023-02-24 18:35:09 +0200

    Sanitize, XSS, Security Headers, Rate Limiting, HPP

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 const path = require("path");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -20,6 +26,7 @@ const courses = require("./routes/courses");
 const auth = require("./routes/auth");
 const users = require("./routes/users");
 const reviews = require("./routes/reviews");
+const { mongo } = require("mongoose");
 
 const app = express();
 
@@ -36,6 +43,28 @@ if (process.env.Node_ENV === "development") {
 
 // File Uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
